8b356ce8ba8c8142e67fc246674d3741547033b0
alexmochu <mochualex4@gmail.com>
2019-11-21 18:47:25 +0300

    setup vuetify, VueRouter

---
+++ resources/js/components/About.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        About Component
+    </div>                    
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('About component')
+        }
+    }
+</script>
