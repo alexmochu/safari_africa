5b29d9e9315c091f0be9b5e63d1a919dd2c4ae4c
alexmochu <mochualex4@gmail.com>
2019-11-22 13:59:09 +0300

    crud fuctionality added on Vue UI

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,13 +1,47 @@
 <template>
-    <div>
-        Home Component
-    </div>                    
+  <div>
+    <h1>Home</h1>
+    <h4>Create A Safari</h4>
+    <form @submit.prevent="addSafari">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Safari Title:</label>
+            <input type="text" class="form-control" v-model="safari.title" />
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Safari Description:</label>
+            <textarea class="form-control" v-model="safari.body" rows="5"></textarea>
+          </div>
+        </div>
+      </div>
+      <br />
+      <div class="form-group">
+        <button class="btn btn-primary">Create</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Home component')
-        }
+export default {
+  data() {
+    return {
+      safari: {}
+    };
+  },
+  methods: {
+    addSafari() {
+      window.axios
+        .post("/api/safari/create", this.safari)
+        .then(response => {
+          this.$router.push({ name: "safaris" });
+        });
     }
+  }
+};
 </script>
