0bac0eb09132eeef0ac75fe7462cab308fef0ed8
alexmochu <mochualex4@gmail.com>
2019-12-02 23:44:58 +0300

    add nav bar and homepage header image

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,13 +1,35 @@
 <template>
-    <div>
-        Home Component
-    </div>                    
+    <v-content>
+        <span>
+            <nav-bar></nav-bar>
+        </span>
+        <v-img class="home-hero" max-height="700">
+            <v-layout class="header-text" justify-center align-center column pa-5>
+                <div class="display-4 font-weight-black white--text text-xs-center">Find Your Perfect Safari</div>
+                <div class="display-1 font-weight-bold white--text text-xs-center">Book incredible safaris around africa</div>
+            </v-layout>
+        </v-img>
+    </v-content>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Home component')
-        }
+import NavBar from '../layouts/NavBar';
+
+export default {
+    name: 'home',
+    components: {
+        NavBar
     }
+};
 </script>
+
+<style scoped>
+    .home-hero {
+        background: url('../images/LandingPageImage.jpg');
+        background-size: cover;
+        height: 70%
+    }
+    .header-text {
+        margin-top: 7%;
+    }
+</style>
---
+++ resources/js/layouts/NavBar.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-app-bar absolute light flat color="rgb(0, 0, 0, 0)">
+      <span class="title ml-3 mr-5">
+        Book&nbsp;
+        <span class="font-weight-light">Safari</span>
+      </span>
+
+      <v-spacer></v-spacer>
+
+      <v-btn text rounded>
+        <router-link class="link" tag="li" to="/">Home</router-link>
+      </v-btn>
+
+      <v-btn text rounded>
+        <router-link class="link" tag="li" to="/safaris">Safaris</router-link>
+      </v-btn>
+
+      <v-btn text rounded>
+        <router-link class="link" tag="li" to="/destinations">Destinations</router-link>
+      </v-btn>
+
+      <v-btn text rounded>
+        <router-link class="link" tag="li" to="/operators">Operators</router-link>
+      </v-btn>
+
+      <v-btn text rounded>
+        <router-link class="link" tag="li" to="/about">About Us</router-link>
+      </v-btn>
+
+      <v-btn text rounded>
+        <router-link class="link" tag="li" to="/login">Login</router-link>
+      </v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+    export default {
+        name: 'NavBar'
+        };
+</script>
+
+<style scoped>
+    .link {
+        list-style-type: none;
+    }
+</style>
